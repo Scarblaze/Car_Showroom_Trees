a08b73127df56d8a6e5066e8c1ed8f8b24f51766
jayeshpatil25 <jayeshpatil7170@gmail.com>
2025-04-01 00:03:14 +0530

    commit

--- dummy.c
+++ dummy.c
@@ -5,15 +5,14 @@
 
 #define ORDER 5
 #define MAX_KEYS (ORDER - 1)
-#define MIN_KEYS (ceil(ORDER/2)-1)
+#define MIN_KEYS (ceil(ORDER / 2) - 1)
 #define MODELS 20
 
-
-
-bool isLeaf(BTreeNode* root)
+bool isLeaf(BTreeNode *root)
 {
-    bool ans= false;
-    if(root->children[0]==NULL) ans=true;
+    bool ans = false;
+    if (root->children[0] == NULL)
+        ans = true;
     return ans;
 }
 
@@ -29,52 +28,64 @@ typedef struct
     int salesperson_id;
 } Car;
 
-typedef struct SoldCarTreeNode {
+typedef struct SoldCarTreeNode
+{
     Car keys[MAX_KEYS];
-    struct SoldCarTreeNode* children[ORDER];
+    struct SoldCarTreeNode *children[ORDER];
     int num_keys;
 } SoldCarTreeNode;
 
-SoldCarTreeNode* create_node_sold_car() {
-    SoldCarTreeNode* node = (SoldCarTreeNode*)malloc(sizeof(SoldCarTreeNode));
+SoldCarTreeNode *create_node_sold_car()
+{
+    SoldCarTreeNode *node = (SoldCarTreeNode *)malloc(sizeof(SoldCarTreeNode));
     node->num_keys = 0;
-    for (int i = 0; i < ORDER; i++) {
+    for (int i = 0; i < ORDER; i++)
+    {
         node->children[i] = NULL;
     }
     return node;
 }
 
-void traverse_sold_car(SoldCarTreeNode* node) {
-    if (node == NULL) return;
-    
+void traverse_sold_car(SoldCarTreeNode *node)
+{
+    if (node == NULL)
+        return;
+
     int i;
-    for (i = 0; i < node->num_keys; i++) {
-        if (node->children[0]) {
+    for (i = 0; i < node->num_keys; i++)
+    {
+        if (node->children[0])
+        {
             traverse_sold_car(node->children[i]);
         }
         printf("%d ", node->keys[i].VIN);
     }
-    
+
     // Traverse the last child
-    if (node->children[0]) {
+    if (node->children[0])
+    {
         traverse_sold_car(node->children[i]);
     }
 }
 
-void split_child_sold_car(SoldCarTreeNode* parent, int child_index) {
-    SoldCarTreeNode* child = parent->children[child_index];
-    SoldCarTreeNode* new_child = create_node_sold_car();
-    int mid_index = MAX_KEYS/2;
+void split_child_sold_car(SoldCarTreeNode *parent, int child_index)
+{
+    SoldCarTreeNode *child = parent->children[child_index];
+    SoldCarTreeNode *new_child = create_node_sold_car();
+    int mid_index = MAX_KEYS / 2;
 
     // Copy upper half of child to new_child
     new_child->num_keys = MAX_KEYS - mid_index - 1;
-    for (int i = 0; i < new_child->num_keys; i++) {
+    for (int i = 0; i < new_child->num_keys; i++)
+    {
         new_child->keys[i] = child->keys[mid_index + 1 + i];
     }
 
     // Copy children if not leaf
-    if (child->children[0]) {
-        for (int i = 0; i <= new_child->num_keys; i++) {
+    if (child->children[0])
+    {
+        for (int i = 0; i <= new_child->num_keys; i++)
+        {
             new_child->children[i] = child->children[mid_index + 1 + i];
         }
     }
@@ -83,7 +94,8 @@ void split_child_sold_car(SoldCarTreeNode* parent, int child_index) {
     child->num_keys = mid_index;
 
     // Make space in parent for new child
-    for (int i = parent->num_keys; i > child_index; i--) {
+    for (int i = parent->num_keys; i > child_index; i--)
+    {
         parent->children[i + 1] = parent->children[i];
         parent->keys[i] = parent->keys[i - 1];
     }
@@ -94,29 +106,37 @@ void split_child_sold_car(SoldCarTreeNode* parent, int child_index) {
     parent->num_keys++;
 }
 
-void insert_non_full_sold_car(SoldCarTreeNode* node, Car key) {
+void insert_non_full_sold_car(SoldCarTreeNode *node, Car key)
+{
     int i = node->num_keys - 1;
 
-    //leaf
-    if (node->children[0]==NULL) {
+    // leaf
+    if (node->children[0] == NULL)
+    {
         // Shift keys and insert
-        while (i >= 0 && key.VIN < node->keys[i].VIN) {
+        while (i >= 0 && key.VIN < node->keys[i].VIN)
+        {
             node->keys[i + 1] = node->keys[i];
             i--;
         }
         node->keys[i + 1] = key;
         node->num_keys++;
-    } else {
+    }
+    else
+    {
         // Find child to insert into
-        while (i >= 0 && key.VIN < node->keys[i].VIN) {
+        while (i >= 0 && key.VIN < node->keys[i].VIN)
+        {
             i--;
         }
-        i++; //because you have to go to child. i was key index until now
+        i++; // because you have to go to child. i was key index until now
 
         // Split if child is full
-        if (node->children[i]->num_keys == MAX_KEYS) {
+        if (node->children[i]->num_keys == MAX_KEYS)
+        {
             split_child_sold_car(node, i);
-            if (key.VIN > node->keys[i].VIN) {
+            if (key.VIN > node->keys[i].VIN)
+            {
                 i++;
             }
         }
@@ -124,16 +144,19 @@ void insert_non_full_sold_car(SoldCarTreeNode* node, Car key) {
     }
 }
 
-void insert_sold_car(SoldCarTreeNode** root, Car key) {
-    if (*root == NULL) {
+void insert_sold_car(SoldCarTreeNode **root, Car key)
+{
+    if (*root == NULL)
+    {
         *root = create_node_sold_car();
         (*root)->keys[0] = key;
         (*root)->num_keys = 1;
         return;
     }
 
-    if ((*root)->num_keys == MAX_KEYS) {
-        SoldCarTreeNode* new_root = create_node_sold_car();
+    if ((*root)->num_keys == MAX_KEYS)
+    {
+        SoldCarTreeNode *new_root = create_node_sold_car();
         new_root->children[0] = *root;
         split_child_sold_car(new_root, 0);
         *root = new_root;
@@ -141,55 +164,64 @@ void insert_sold_car(SoldCarTreeNode** root, Car key) {
     insert_non_full_sold_car(*root, key);
 }
 
-
-
-typedef struct AvailableCarTreeNode {
+typedef struct AvailableCarTreeNode
+{
     Car keys[MAX_KEYS];
     struct AvailableCarTreeNode *children[ORDER];
-    int num_keys; 
+    int num_keys;
 } AvailableCarTreeNode;
 
-
-SoldCarTreeNode* create_node_available_car() {
-    AvailableCarTreeNode* node = (AvailableCarTreeNode*)malloc(sizeof(AvailableCarTreeNode));
+SoldCarTreeNode *create_node_available_car()
+{
+    AvailableCarTreeNode *node = (AvailableCarTreeNode *)malloc(sizeof(AvailableCarTreeNode));
     node->num_keys = 0;
-    for (int i = 0; i < ORDER; i++) {
+    for (int i = 0; i < ORDER; i++)
+    {
         node->children[i] = NULL;
     }
     return node;
 }
 
-void traverse_available_car(AvailableCarTreeNode* node) {
-    if (node == NULL) return;
-    
+void traverse_available_car(AvailableCarTreeNode *node)
+{
+    if (node == NULL)
+        return;
+
     int i;
-    for (i = 0; i < node->num_keys; i++) {
-        if (node->children[0]) {
+    for (i = 0; i < node->num_keys; i++)
+    {
+        if (node->children[0])
+        {
             traverse_available_car(node->children[i]);
         }
         printf("%d ", node->keys[i].VIN);
     }
-    
+
     // Traverse the last child
-    if (node->children[0]) {
+    if (node->children[0])
+    {
         traverse_available_car(node->children[i]);
     }
 }
 
-void split_child_available_car(AvailableCarTreeNode* parent, int child_index) {
-    AvailableCarTreeNode* child = parent->children[child_index];
-    AvailableCarTreeNode* new_child = create_node_available_car();
-    int mid_index = MAX_KEYS/2;
+void split_child_available_car(AvailableCarTreeNode *parent, int child_index)
+{
+    AvailableCarTreeNode *child = parent->children[child_index];
+    AvailableCarTreeNode *new_child = create_node_available_car();
+    int mid_index = MAX_KEYS / 2;
 
     // Copy upper half of child to new_child
     new_child->num_keys = MAX_KEYS - mid_index - 1;
-    for (int i = 0; i < new_child->num_keys; i++) {
+    for (int i = 0; i < new_child->num_keys; i++)
+    {
         new_child->keys[i] = child->keys[mid_index + 1 + i];
     }
 
     // Copy children if not leaf
-    if (child->children[0]) {
-        for (int i = 0; i <= new_child->num_keys; i++) {
+    if (child->children[0])
+    {
+        for (int i = 0; i <= new_child->num_keys; i++)
+        {
             new_child->children[i] = child->children[mid_index + 1 + i];
         }
     }
@@ -198,7 +230,8 @@ void split_child_available_car(AvailableCarTreeNode* parent, int child_index) {
     child->num_keys = mid_index;
 
     // Make space in parent for new child
-    for (int i = parent->num_keys; i > child_index; i--) {
+    for (int i = parent->num_keys; i > child_index; i--)
+    {
         parent->children[i + 1] = parent->children[i];
         parent->keys[i] = parent->keys[i - 1];
     }
@@ -209,29 +242,37 @@ void split_child_available_car(AvailableCarTreeNode* parent, int child_index) {
     parent->num_keys++;
 }
 
-void insert_non_full_available_car(AvailableCarTreeNode* node, Car key) {
+void insert_non_full_available_car(AvailableCarTreeNode *node, Car key)
+{
     int i = node->num_keys - 1;
 
-    //leaf
-    if (node->children[0]==NULL) {
+    // leaf
+    if (node->children[0] == NULL)
+    {
         // Shift keys and insert
-        while (i >= 0 && key.VIN < node->keys[i].VIN) {
+        while (i >= 0 && key.VIN < node->keys[i].VIN)
+        {
             node->keys[i + 1] = node->keys[i];
             i--;
         }
         node->keys[i + 1] = key;
         node->num_keys++;
-    } else {
+    }
+    else
+    {
         // Find child to insert into
-        while (i >= 0 && key.VIN < node->keys[i].VIN) {
+        while (i >= 0 && key.VIN < node->keys[i].VIN)
+        {
             i--;
         }
-        i++; //because you have to go to child. i was key index until now
+        i++; // because you have to go to child. i was key index until now
 
         // Split if child is full
-        if (node->children[i]->num_keys == MAX_KEYS) {
+        if (node->children[i]->num_keys == MAX_KEYS)
+        {
             split_child_available_car(node, i);
-            if (key.VIN > node->keys[i].VIN) {
+            if (key.VIN > node->keys[i].VIN)
+            {
                 i++;
             }
         }
@@ -239,16 +280,19 @@ void insert_non_full_available_car(AvailableCarTreeNode* node, Car key) {
     }
 }
 
-void insert_available_car(AvailableCarTreeNode** root, Car key) {
-    if (*root == NULL) {
+void insert_available_car(AvailableCarTreeNode **root, Car key)
+{
+    if (*root == NULL)
+    {
         *root = create_node_available_car();
         (*root)->keys[0] = key;
         (*root)->num_keys = 1;
         return;
     }
 
-    if ((*root)->num_keys == MAX_KEYS) {
-        AvailableCarTreeNode* new_root = create_node_available_car();
+    if ((*root)->num_keys == MAX_KEYS)
+    {
+        AvailableCarTreeNode *new_root = create_node_available_car();
         new_root->children[0] = *root;
         split_child_available_car(new_root, 0);
         *root = new_root;
@@ -256,18 +300,17 @@ void insert_available_car(AvailableCarTreeNode** root, Car key) {
     insert_non_full_available_car(*root, key);
 }
 
-
-
-//refrence 
-typedef struct BTreeNode {
+// refrence
+typedef struct BTreeNode
+{
     int keys[MAX_KEYS];
-    struct BTreeNode* children[ORDER];
+    struct BTreeNode *children[ORDER];
     int num_keys;
 } BTreeNode;
 
 typedef struct
 {
-    int customer_id;
+    int id;
     char name[50];
     char mobile[15];
     char address[100];
@@ -276,14 +319,141 @@ typedef struct
     char payment_type[20];
 } Customer;
 
-typedef struct CustomerTreeNode {
-    int keys[ORDER - 1];
-    Customer customer;
-    struct CustomerTreeNode *children_ptr[ORDER];
-    int numKeys;
-    int leaf;
+typedef struct CustomerTreeNode
+{
+    Customer keys[MAX_KEYS];
+    struct CustomerTreeNode *children[ORDER];
+    int num_keys;
 } CustomerTreeNode;
 
+CustomerTreeNode *create_node_customer()
+{
+    CustomerTreeNode *node = (CustomerTreeNode *)malloc(sizeof(CustomerTreeNode));
+    node->num_keys = 0;
+    for (int i = 0; i < ORDER; i++)
+    {
+        node->children[i] = NULL;
+    }
+    return node;
+}
+
+void traverse_customer(CustomerTreeNode *node)
+{
+    if (node == NULL)
+        return;
+
+    int i;
+    for (i = 0; i < node->num_keys; i++)
+    {
+        if (node->children[0])
+        {
+            traverse_customer(node->children[i]);
+        }
+        printf("%d ", node->keys[i].id);
+    }
+
+    // Traverse the last child
+    if (node->children[0])
+    {
+        traverse_customer(node->children[i]);
+    }
+}
+
+void split_child_customer(CustomerTreeNode *parent, int child_index)
+{
+    CustomerTreeNode *child = parent->children[child_index];
+    CustomerTreeNode *new_child = create_node_customer();
+    int mid_index = MAX_KEYS / 2;
+
+    // Copy upper half of child to new_child
+    new_child->num_keys = MAX_KEYS - mid_index - 1;
+    for (int i = 0; i < new_child->num_keys; i++)
+    {
+        new_child->keys[i] = child->keys[mid_index + 1 + i];
+    }
+
+    // Copy children if not leaf
+    if (child->children[0])
+    {
+        for (int i = 0; i <= new_child->num_keys; i++)
+        {
+            new_child->children[i] = child->children[mid_index + 1 + i];
+        }
+    }
+
+    // Reduce keys in original child
+    child->num_keys = mid_index;
+
+    // Make space in parent for new child
+    for (int i = parent->num_keys; i > child_index; i--)
+    {
+        parent->children[i + 1] = parent->children[i];
+        parent->keys[i] = parent->keys[i - 1];
+    }
+
+    // Move middle key to parent and link new child
+    parent->children[child_index + 1] = new_child;
+    parent->keys[child_index] = child->keys[mid_index];
+    parent->num_keys++;
+}
+
+void insert_non_full_customer(CustomerTreeNode *node, Customer key)
+{
+    int i = node->num_keys - 1;
+
+    // leaf
+    if (node->children[0] == NULL)
+    {
+        // Shift keys and insert
+        while (i >= 0 && key.id < node->keys[i].id)
+        {
+            node->keys[i + 1] = node->keys[i];
+            i--;
+        }
+        node->keys[i + 1] = key;
+        node->num_keys++;
+    }
+    else
+    {
+        // Find child to insert into
+        while (i >= 0 && key.id < node->keys[i].id)
+        {
+            i--;
+        }
+        i++; // because you have to go to child. i was key index until now
+
+        // Split if child is full
+        if (node->children[i]->num_keys == MAX_KEYS)
+        {
+            split_child_customer(node, i);
+            if (key.id > node->keys[i].id)
+            {
+                i++;
+            }
+        }
+        insert_non_full_customer(node->children[i], key);
+    }
+}
+
+void insert_customer(CustomerTreeNode **root, Customer key)
+{
+    if (*root == NULL)
+    {
+        *root = create_node_customer();
+        (*root)->keys[0] = key;
+        (*root)->num_keys = 1;
+        return;
+    }
+
+    if ((*root)->num_keys == MAX_KEYS)
+    {
+        CustomerTreeNode *new_root = create_node_customer();
+        new_root->children[0] = *root;
+        split_child_customer(new_root, 0);
+        *root = new_root;
+    }
+    insert_non_full_customer(*root, key);
+}
 typedef struct
 {
     int id;
@@ -294,28 +464,155 @@ typedef struct
     SoldCarTreeNode *root;
 } SalesPerson;
 
-typedef struct SalesTreeNode {
-    int keys[ORDER - 1]; 
-    SalesPerson salesPersons;
+typedef struct SalesTreeNode
+{
+    SalesPerson keys[MAX_KEYS];
     struct SalesTreeNode *children[ORDER];
-    int numKeys;
-    int leaf;
+    int num_keys;
 } SalesTreeNode;
 
-typedef struct 
+SalesTreeNode *create_node_sales_person()
+{
+    SalesTreeNode *node = (SalesTreeNode *)malloc(sizeof(SalesTreeNode));
+    node->num_keys = 0;
+    for (int i = 0; i < ORDER; i++)
+    {
+        node->children[i] = NULL;
+    }
+    return node;
+}
+
+void traverse_sales_person(SalesTreeNode *node)
+{
+    if (node == NULL)
+        return;
+
+    int i;
+    for (i = 0; i < node->num_keys; i++)
+    {
+        if (node->children[0])
+        {
+            traverse_sales_person(node->children[i]);
+        }
+        printf("%d ", node->keys[i].id);
+    }
+
+    // Traverse the last child
+    if (node->children[0])
+    {
+        traverse_sales_person(node->children[i]);
+    }
+}
+
+void split_child_sales_person(SalesTreeNode *parent, int child_index)
+{
+    SalesTreeNode *child = parent->children[child_index];
+    SalesTreeNode *new_child = create_node_sales_person();
+    int mid_index = MAX_KEYS / 2;
+
+    // Copy upper half of child to new_child
+    new_child->num_keys = MAX_KEYS - mid_index - 1;
+    for (int i = 0; i < new_child->num_keys; i++)
+    {
+        new_child->keys[i] = child->keys[mid_index + 1 + i];
+    }
+
+    // Copy children if not leaf
+    if (child->children[0])
+    {
+        for (int i = 0; i <= new_child->num_keys; i++)
+        {
+            new_child->children[i] = child->children[mid_index + 1 + i];
+        }
+    }
+
+    // Reduce keys in original child
+    child->num_keys = mid_index;
+
+    // Make space in parent for new child
+    for (int i = parent->num_keys; i > child_index; i--)
+    {
+        parent->children[i + 1] = parent->children[i];
+        parent->keys[i] = parent->keys[i - 1];
+    }
+
+    // Move middle key to parent and link new child
+    parent->children[child_index + 1] = new_child;
+    parent->keys[child_index] = child->keys[mid_index];
+    parent->num_keys++;
+}
+
+void insert_non_full_sales_person(SalesTreeNode *node, SalesPerson key)
+{
+    int i = node->num_keys - 1;
+
+    // leaf
+    if (node->children[0] == NULL)
+    {
+        // Shift keys and insert
+        while (i >= 0 && key.id < node->keys[i].id)
+        {
+            node->keys[i + 1] = node->keys[i];
+            i--;
+        }
+        node->keys[i + 1] = key;
+        node->num_keys++;
+    }
+    else
+    {
+        // Find child to insert into
+        while (i >= 0 && key.id < node->keys[i].id)
+        {
+            i--;
+        }
+        i++; // because you have to go to child. i was key index until now
+
+        // Split if child is full
+        if (node->children[i]->num_keys == MAX_KEYS)
+        {
+            split_child_sales_person(node, i);
+            if (key.id > node->keys[i].id)
+            {
+                i++;
+            }
+        }
+        insert_non_full_sales_person(node->children[i], key);
+    }
+}
+
+void insert_sales_person(SalesTreeNode **root, SalesPerson key)
+{
+    if (*root == NULL)
+    {
+        *root = create_node_sales_person();
+        (*root)->keys[0] = key;
+        (*root)->num_keys = 1;
+        return;
+    }
+
+    if ((*root)->num_keys == MAX_KEYS)
+    {
+        SalesTreeNode *new_root = create_node_sales_person();
+        new_root->children[0] = *root;
+        split_child_sales_person(new_root, 0);
+        *root = new_root;
+    }
+    insert_non_full_sales_person(*root, key);
+}
+
+typedef struct
 {
     char car_model[20];
     int sold_cars;
-    int available_cars;  
+    int available_cars;
 } stock_details;
 
 typedef struct showroom
 {
     int showroom_id;
-    SalesTreeNode* sales_root;
-    CustomerTreeNode* customer_root;
-    AvailableCarTreeNode* available_car_root;
-    SoldCarTreeNode* sold_car_root;
+    SalesTreeNode *sales_root;
+    CustomerTreeNode *customer_root;
+    AvailableCarTreeNode *available_car_root;
+    SoldCarTreeNode *sold_car_root;
     stock_details stock[MODELS];
 };
-
